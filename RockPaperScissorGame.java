fc9dd698b81ac0315e9293ce8e7ab0323255570c
AkramShaik2903 <132455866+AkramShaik2903@users.noreply.github.com>
2024-11-23 15:48:32 +0530

    Add files via upload

--- RockPaperScissorGame.java
+++ RockPaperScissorGame.java
@@ -7,54 +7,25 @@ public class RockPaperScissorGame {
         int i=1,count=0;
         System.out.println("ROCK,PAPER & SCISSORS");
         System.out.println("Rock : 0\tPaper : 1\tScissor : 2");
-        while(i<=5)
+        int a=rand.nextInt(3);
+        System.out.print("Enter your Choice : ");
+        int b=sc.nextInt();
+        System.out.println("Computer Choice : "+a);
+        if(a==b)
         {
-            int a=rand.nextInt(3);
-            System.out.print("Enter your Choice : ");
-            int b=sc.nextInt();
-            System.out.println("Computer Choice : "+a);
-            if(a==b)
-            {
-                System.out.println("Match Draw!");
-            }
-            else if(a==0 && b==1)
-            {
-                System.out.println("User Wins!");
-                count++;
-            }
-            else if(a==0 && b==2)
-            {
-                System.out.println("Computer Wins!");
-            }
-            else if(a==1 && b==0)
-            {
-                System.out.println("Computer Wins!");
-            }
-            else if(a==1 && b==2)
-            {
-                System.out.println("User Wins!");
-                count++;
-            }
-            else if(a==2 && b==0)
-            {
-                System.out.println("User Wins!");
-                count++;
-            }
-            else if(a==2 && b==1) {
-                System.out.println("Computer Wins!");
-            }
-            else {
-                System.out.println("Invalid Option!");
-            }
-            i++;
+            System.out.println("Match Draw!");
         }
-        if(count>2)
+        else if(a==0 && b==1 || a==1 && b==2 || a==2 && b==0)
         {
-            System.out.println("User won the match!");
+            System.out.println("User Wins!");
+        }
+        else if(a==0 && b==2 || a==1 && b==0 || a==2 && b==1)
+        {
+            System.out.println("Computer Wins!");
         }
         else
         {
-            System.out.println("User lost the match,try again...!");
+            System.out.println("Invalid Option!");
         }
     }
 }
